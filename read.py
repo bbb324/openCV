62f13a4eac998cea1ceb3baa55cd8ec6696f2072
bbb324 <johnigzie@gmail.com>
2023-06-22 22:43:17 +0800

    add readme

--- read.py
+++ read.py
@@ -6,10 +6,11 @@ img = cv.imread('Photos/cat.jpg')
 # cv.imshow('Cat', img)
 # cv.imgshow('Cat', img)
 
-# read Videos
+# read Videos 读取一个视频小狗，如果小狗离开，则关闭视频
 capture = cv.VideoCapture('Videos/dog.mp4')
 while True:
-    isTrue, frame = capture.read
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
     if(cv.waitKey(20) & 0xFF==ord('d')):
         break
 capture.release()
